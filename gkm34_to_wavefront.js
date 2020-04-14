a1b10732c4a62144b19cd961aa5db6d76a062242
danielbartsch <daniel.bartsch@adverity.com>
2020-04-14 22:21:57 +0200

    took 'noData' into consideration, which can sometimes happen if there's no height data available realworld example would be inside of vienna at https://atlas.noe.gv.at/

--- gkm34_to_wavefront.js
+++ gkm34_to_wavefront.js
@@ -28,11 +28,24 @@ fs.readFile(inputFileName, "utf8", (error, data) => {
   if (error) {
     console.error(error.message)
   } else {
-    const lines = data.replace(/\r/g, "").split("\n")
+    const lines = data
+      .replace(/\r/g, "")
+      .split("\n")
+      .filter((line) => line)
+
+    const filteredForNoData = lines.filter((line) => !line.includes("nodata"))
+
+    if (filteredForNoData.length !== lines.length) {
+      console.log(
+        `Filtered out ${lines.length - filteredForNoData.length} of ${
+          lines.length
+        } points without any height data`
+      )
+    }
+
     fs.writeFile(
       outputFileName,
-      lines
-        .filter((line) => line)
+      filteredForNoData
         .map((line) => {
           const [pointCount, x, y, surface, terrain] = line.split("\t")
           return `v ${toFloat(y)} ${
