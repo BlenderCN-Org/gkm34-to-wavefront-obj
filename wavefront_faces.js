9ec67f6d65b4bceccff8303f1ba871010dd86c8e
danielbartsch <daniel.bartsch@adverity.com>
2020-04-18 00:59:38 +0200

    added circumcenter calculation of a 3d triangle for preparation of the delaunay algorithm source: https://www.ics.uci.edu/~eppstein/junkyard/circumcenter.html

--- wavefront_faces.js
+++ wavefront_faces.js
@@ -16,6 +16,44 @@ const getDistance = ({ x: x1, y: y1, z: z1 }, { x: x2, y: y2, z: z2 }) =>
     Math.abs(x2 - x1) ** 2 + Math.abs(y2 - y1) ** 2 + Math.abs(z2 - z1) ** 2
   )
 
+const getCircumCenter = ([a, b, c]) => {
+  /* Use coordinates relative to point `a' of the triangle. */
+  const baX = b.x - a.x
+  const baY = b.y - a.y
+  const baZ = b.z - a.z
+  const caX = c.x - a.x
+  const caY = c.y - a.y
+  const caZ = c.z - a.z
+
+  /* Squares of lengths of the edges incident to `a'. */
+  const baLength = baX * baX + baY * baY + baZ * baZ
+  const caLength = caX * caX + caY * caY + caZ * caZ
+
+  /* Cross product of these edges. */
+  const xCrossBC = baY * caZ - caY * baZ
+  const yCrossBC = baZ * caX - caZ * baX
+  const zCrossBC = baX * caY - caX * baY
+
+  /* Calculate the denominator of the formulae. */
+  const denominator =
+    0.5 / (xCrossBC * xCrossBC + yCrossBC * yCrossBC + zCrossBC * zCrossBC)
+
+  /* Calculate offset (from `a') of circumcenter. */
+  return {
+    x:
+      ((baLength * caY - caLength * baY) * zCrossBC -
+        (baLength * caZ - caLength * baZ) * yCrossBC) *
+      denominator,
+    y:
+      ((baLength * caZ - caLength * baZ) * xCrossBC -
+        (baLength * caX - caLength * baX) * zCrossBC) *
+      denominator,
+    z:
+      ((baLength * caX - caLength * baX) * yCrossBC -
+        (baLength * caY - caLength * baY) * xCrossBC) *
+      denominator,
+  }
+}
 fs.readFile(inputFileName, "utf8", (error, data) => {
   if (error) {
     console.error(error.message)
